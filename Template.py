cb130affd68434a53e7e18c172f789aa54a0d851
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-07 22:06:27 +1300

    Initial Commit: formatted

---
+++ Template.py
@@ -0,0 +1,14 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
+if __name__ == "__main__":
+    logging.info("Started Program")
+    
