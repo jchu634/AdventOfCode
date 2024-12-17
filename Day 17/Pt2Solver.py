9555fb8e12e5419b035d8e467b9655712e059af3
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-18 05:47:43 +1300

    Pt2 Solution

--- Day 17/Pt2Solver.py
+++ Day 17/Pt2Solver.py
@@ -12,89 +12,40 @@ logging.basicConfig(
 )
 
 
-def chunkify(lst, n):
-    return [lst[i::n] for i in range(n)]
-
-
-def getComboOperand(operand, regA, regB, regC):
-    if operand <= 3:
-        return operand
-    elif operand == 4:
-        return regA
-    elif operand == 5:
-        return regB
-    elif operand == 6:
-        return regC
-
-
-def getProgramOutput(regA, regB, regC, Program):
+def programLoop(a):
     output = []
-
-    instructionPointer = 0
-    try:
-        while True:
-            opCode = Program[instructionPointer]
-            operand = Program[instructionPointer+1]
-            match opCode:
-                case 0:  # adv
-                    regA = regA // (2**getComboOperand(operand, regA, regB, regC))
-                case 1:  # bxl
-                    regB = regB ^ operand
-                case 2:  # bst
-                    regB = getComboOperand(operand, regA, regB, regC) % 8
-                case 3:  # jnz
-                    if regA != 0:
-                        instructionPointer = operand
-                        continue
-                case 4:  # bxc
-                    regB = regB ^ regC
-                case 5:  # out
-                    output.append(getComboOperand(operand, regA, regB, regC) % 8)
-                case 6:  # bdv
-                    regB = regA // (2**getComboOperand(operand, regA, regB, regC))
-                case 7:  # cdv
-                    regC = regA // (2**getComboOperand(operand, regA, regB, regC))
-
-            instructionPointer += 2
-    except IndexError:
-        pass
-    return output
-
-
-def generator():
     while True:
-        yield
+        b = a % 8
+        b = b ^ 4
+        c = a // (2**b)
+        b = b ^ c
+        b = b ^ 4
+        output.append(b % 8)
+        a = a // 8
+        if a == 0:
+            return output
 
 
 def getAns():
     with open("input.txt", "r") as file:
         regA, regB, regC, _, Program = file.read().splitlines()
 
-    regA = 0
-    regB = int(regB.split(": ")[1])
-    regC = int(regC.split(": ")[1])
+    # regA = int(regA.split(": ")[1])
+    # regB = int(regB.split(": ")[1])
+    # regC = int(regC.split(": ")[1])
     Program = [int(i) for i in Program.split(": ")[1].split(",")]
-    logging.info(Program)
-    y = len(Program)
-    x = len(Program)-3
-    while True:
 
-        output = getProgramOutput(regA, regB, regC, Program)
-        matching_digits = sum(1 for a, b in zip(output, Program) if a == b)
+    possibleCombinations = [0]  # Inti
+    for i in range(1, len(Program)+1):
+        newCombinations = []
+        for com in possibleCombinations:
+            for offset in range(8):
+                a = 8 * com + offset
+                if programLoop(a) == Program[-i:]:
+                    newCombinations.append(a)
+        possibleCombinations = newCombinations
 
-        if output[x:y+1] == Program[x:y]:
-
-            logging.info(f"{output} ({regA}): {matching_digits} matching digits")
-            output_str = ' '.join(bin(x)[2:] if i % 2 == 0 else str(x) for i, x in enumerate(output))
-
-            # logging.info(f"Output: {output_str}")
-
-        if (output == Program):
-            logging.info(getProgramOutput(regA, regB, regC, Program))
-            logging.info(getProgramOutput(regA, regB, regC, Program) == Program)
-            return regA
-
-        regA += 1
+    return min(possibleCombinations)
 
 
 if __name__ == "__main__":
